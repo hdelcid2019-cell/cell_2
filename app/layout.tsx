8c4468723c8dff2da9fe00e9632236c448622086
Hector <hdelcid2019@gmail.com>
2025-11-02 20:08:05 -0500

    add sign-in/sign-up routes with Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <ClerkProvider>
-      <html lang="en"><body>{children}</body></html>
-    </ClerkProvider>
-  );
+  return <ClerkProvider><html lang="en"><body>{children}</body></html></ClerkProvider>;
 }
